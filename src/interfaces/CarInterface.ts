b50e43202466aeb7f56b65545d2050ce2f2b4e74
renato <renatowillian.2000@gmail.com>
2022-07-14 08:11:26 -0300

    ♻ refactor: add zod in interfaces

--- src/interfaces/CarInterface.ts
+++ src/interfaces/CarInterface.ts
@@ -1,6 +1,9 @@
-import { Vehicle } from './VehicleInterface';
+import { z } from 'zod';
+import { VehicleSchema } from './VehicleInterface';
 
-export interface Car extends Vehicle {
-  doorsQty: number;
-  seatsQty: number;
-}
+export const CarSchema = VehicleSchema.extend({
+  doorsQty: z.number().min(2).max(4),
+  seatsQty: z.number().min(2).max(7),
+});
+
+export type Car = z.infer<typeof CarSchema>;
