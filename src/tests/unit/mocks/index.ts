70321ab01432c19d588bb2680692b66101fd9dc2
renato <renatowillian.2000@gmail.com>
2022-07-14 12:51:46 -0300

    🤡 test: add mock car

---
+++ src/tests/unit/mocks/index.ts
@@ -0,0 +1,22 @@
+import { CarDoc } from "../../../models/schemas/CarsSchemas";
+
+export const mockNewCar = {
+  model: "Fiat Uno",
+  year: 1963,
+  color: "blue",
+  buyValue: 3500,
+  seatsQty: 4,
+  doorsQty: 4,
+  __v: 0
+} as CarDoc;
+
+export const mockCreateCar = {
+  _id: "4edd40c86762e0fb12000003",
+  model: "Fiat Uno",
+  year: 1963,
+  color: "blue",
+  buyValue: 3500,
+  seatsQty: 4,
+  doorsQty: 4,
+  __v: 0
+} as CarDoc;
